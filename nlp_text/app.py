f3a22b1bbb0e69a51002b27c1848da3e1d157619
timmyyeh <timmyyeh@cs.washington.edu>
2019-02-03 22:27:32 -0800

    done with nlp

--- nlp_text/app.py
+++ nlp_text/app.py
@@ -5,6 +5,7 @@ from flask import (
 )
 from pymongo import MongoClient
 import bcrypt
+import spacy
 
 app = Flask(__name__)
 
@@ -41,6 +42,49 @@ def register():
         'status': 200,
         'success': True
     })
+
+@app.route('/detect')
+def detect():
+    data = request.get_json()
+
+    # verify user
+    if not verifyUser(data['username'], data['password']):
+        return jsonify({
+            'status': 302,
+            'message': 'Invalid username or password'
+        })
+
+    # compare text
+    nlp = spacy.load('en_core_web_sm')
+    text1 = nlp(data['text1'])
+    text2 = nlp(data['text2'])
+
+    user = User.find({'username': data['username']})[0]
+    User.update({
+        'username': data['username']
+    }, {
+        '$set': {'tokens': user['tokens'] - 1}
+    })
+
+    return jsonify({
+        'status': 200,
+        'sucess': True,
+        'similarity': text1.similarity(text2)
+    })
+
+def verifyUser(username, passowrd):
+    if not userExist(username):
+        print(username)
+        print('user not exist')
+        return False
+    
+    hPass = User.find({'username': username})[0]['password']
+
+    if not bcrypt.hashpw(passowrd, hPass) == hPass:
+        print('password not match')
+        return False
+
+    return True
 
 def userExist(username):
     if User.find({'username': username}).count() == 1:
