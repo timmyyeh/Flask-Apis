4fa8ddfb68cfe4ababef2c85819f52fec04fd932
timmyyeh <timmyyeh@cs.washington.edu>
2019-02-06 23:53:33 -0800

    done with tensorflow image classification

--- image_classification/app.py
+++ image_classification/app.py
@@ -5,6 +5,9 @@ from flask import (
 )
 
 from pymongo import MongoClient
+import subprocess
+import json
+import requests
 
 app = Flask(__name__)
 client = MongoClient('localhost', 27017)
@@ -15,21 +18,36 @@ Image = db['Images']
 def index():
     return 'ok'
 
-@app.route('/register')
+@app.route('/register', methods=['POST'])
 def register():
     data = request.get_json()
 
     res = Image.insert_one({
         'username': data['username'],
         'password': data['password'],
-        'toke_amount': data['token_amount']
+        'toke_amount': 5
     })
 
     return jsonify({
         'success': True,
-        'data': res
     })
 
+@app.route('/classify')
+def classify():
+    data = request.get_json()
+    url = data['url']
+
+    r = requests.get(url)
+    ret = {}
+    with open("temp.jpg", "wb") as f:
+        f.write(r.content)
+        proc = subprocess.Popen('python classify_image.py --image_file=temp.jpg', shell=True)
+        proc.communicate()[0]
+        proc.wait()
+        with open('text.txt') as g:
+            ret = json.load(g)
+    
+    return jsonify(ret)
 
 if __name__ == "__main__":
     app.run()
